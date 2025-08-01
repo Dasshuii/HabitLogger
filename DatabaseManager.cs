0c21d3b5aa9fa110c68b86c136dbcf98b4def8ab
Daniel Ramirez <daniel.ramirzc22@gmail.com>
2025-08-01 01:35:03 -0600

    Database Manager first approach

---
+++ DatabaseManager.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.Sqlite;
+
+
+namespace HabitLogger
+{
+    internal class DatabaseManager {
+        const string connectionString = @"Data Source=C:\Users\dassh\source\repos\HabitLogger\HabitLogger\habit_logger.db";
+
+        public void CreateTables()
+        {
+            const string queryString = "CREATE TABLE IF NOT EXISTS drinking_water (" +
+                "id INTEGER NOT NULL PRIMARY KEY," +
+                "date TEXT NOT NULL," +
+                "quantity INTEGER NOT NULL" +
+                ");";
+
+            using (SqliteConnection conn = new(connectionString))
+            {
+                conn.Open();
+                var tableCmd = conn.CreateCommand();
+
+                tableCmd.CommandText = queryString;
+                tableCmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using HabitLogger;
+using SQLitePCL;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Batteries.Init();
+        DatabaseManager databaseManager = new DatabaseManager();
+        databaseManager.CreateTables();
+    }
+}
